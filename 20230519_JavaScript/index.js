47e753b801154936b296dd898bc20705b16b4f10
projectsheeo <132822946+projectsheeo@users.noreply.github.com>
2023-05-19 10:24:30 +0900

    Add files via upload

---
+++ 20230519_JavaScript/index.js
@@ -0,0 +1,314 @@
+"use strict";
+
+// 各ボタンに名前をつける
+let btn01 = document.getElementById("btn01");
+let btn02 = document.getElementById("btn02");
+let btn03 = document.getElementById("btn03");
+let btn04 = document.getElementById("btn04");
+let btn05 = document.getElementById("btn05");
+let btn06 = document.getElementById("btn06");
+let btn07 = document.getElementById("btn07");
+let btn08 = document.getElementById("btn08");
+let btn09 = document.getElementById("btn09");
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12 = document.getElementById("btn12");
+let btn13 = document.getElementById("btn13");
+let btn14 = document.getElementById("btn14");
+let btn15 = document.getElementById("btn15");
+let btn16 = document.getElementById("btn16");
+let btn17 = document.getElementById("btn17");
+let btn18 = document.getElementById("btn18");
+let btn19 = document.getElementById("btn19");
+let btn20 = document.getElementById("btn20");
+let btn21 = document.getElementById("btn21");
+let btn22 = document.getElementById("btn22");
+let btn23 = document.getElementById("btn23");
+let btn24 = document.getElementById("btn24");
+let btn25 = document.getElementById("btn25");
+let btn26 = document.getElementById("btn26");
+let btn27 = document.getElementById("btn27");
+let btn28 = document.getElementById("btn28");
+let btn29 = document.getElementById("btn29");
+let btn30 = document.getElementById("btn30");
+let btn31 = document.getElementById("btn31");
+let btn32 = document.getElementById("btn32");
+let btn33 = document.getElementById("btn33");
+let btn34 = document.getElementById("btn34");
+let btn35 = document.getElementById("btn35");
+let btn36 = document.getElementById("btn36");
+let btn37 = document.getElementById("btn37");
+let btn38 = document.getElementById("btn38");
+let btn39 = document.getElementById("btn39");
+let btn40 = document.getElementById("btn40");
+let btn41 = document.getElementById("btn41");
+let btn42 = document.getElementById("btn42");
+let btn43 = document.getElementById("btn43");
+let btn44 = document.getElementById("btn44");
+let btn45 = document.getElementById("btn45");
+let btn46 = document.getElementById("btn46");
+let btn47 = document.getElementById("btn47");
+let btn48 = document.getElementById("btn48");
+let btn49 = document.getElementById("btn49");
+let btn50 = document.getElementById("btn50");
+let btn51 = document.getElementById("btn51");
+let btn52 = document.getElementById("btn52");
+let btn53 = document.getElementById("btn53");
+let btn54 = document.getElementById("btn54");
+let btn55 = document.getElementById("btn55");
+let btn56 = document.getElementById("btn56");
+let btn57 = document.getElementById("btn57");
+let btn58 = document.getElementById("btn58");
+let btn59 = document.getElementById("btn59");
+let btn60 = document.getElementById("btn60");
+let btn61 = document.getElementById("btn61");
+let btn62 = document.getElementById("btn62");
+let btn63 = document.getElementById("btn63");
+let btn64 = document.getElementById("btn64");
+let btn65 = document.getElementById("btn65");
+let btn66 = document.getElementById("btn66");
+let btn67 = document.getElementById("btn67");
+
+// 各ボタンを押したら、クラスを付け外しする（白&黄色）
+btn01.addEventListener("click", function () {
+    btn01.classList.toggle('clicked');
+});
+btn02.addEventListener("click", function () {
+    btn02.classList.toggle('clicked');
+});
+btn03.addEventListener("click", function () {
+    btn03.classList.toggle('clicked');
+});
+btn04.addEventListener("click", function () {
+    btn04.classList.toggle('clicked');
+});
+btn05.addEventListener("click", function () {
+    btn05.classList.toggle('clicked');
+});
+btn06.addEventListener("click", function () {
+    btn06.classList.toggle('clicked');
+});
+btn07.addEventListener("click", function () {
+    btn07.classList.toggle('clicked');
+});
+btn08.addEventListener("click", function () {
+    btn08.classList.toggle('clicked');
+});
+btn09.addEventListener("click", function () {
+    btn09.classList.toggle('clicked');
+});
+btn10.addEventListener("click", function () {
+    btn10.classList.toggle('clicked');
+});
+btn11.addEventListener("click", function () {
+    btn11.classList.toggle('clicked');
+});
+btn12.addEventListener("click", function () {
+    btn12.classList.toggle('clicked');
+});
+btn13.addEventListener("click", function () {
+    btn13.classList.toggle('clicked');
+});
+btn14.addEventListener("click", function () {
+    btn14.classList.toggle('clicked');
+});
+btn15.addEventListener("click", function () {
+    btn15.classList.toggle('clicked');
+});
+btn16.addEventListener("click", function () {
+    btn16.classList.toggle('clicked');
+});
+btn17.addEventListener("click", function () {
+    btn17.classList.toggle('clicked');
+});
+btn18.addEventListener("click", function () {
+    btn18.classList.toggle('clicked');
+});
+btn19.addEventListener("click", function () {
+    btn19.classList.toggle('clicked');
+});
+btn20.addEventListener("click", function () {
+    btn20.classList.toggle('clicked');
+});
+btn21.addEventListener("click", function () {
+    btn21.classList.toggle('clicked');
+});
+btn22.addEventListener("click", function () {
+    btn22.classList.toggle('clicked');
+});
+btn23.addEventListener("click", function () {
+    btn23.classList.toggle('clicked');
+});
+btn24.addEventListener("click", function () {
+    btn24.classList.toggle('clicked');
+});
+btn25.addEventListener("click", function () {
+    btn25.classList.toggle('clicked');
+});
+btn26.addEventListener("click", function () {
+    btn26.classList.toggle('clicked');
+});
+btn27.addEventListener("click", function () {
+    btn27.classList.toggle('clicked');
+});
+btn28.addEventListener("click", function () {
+    btn28.classList.toggle('clicked');
+});
+btn29.addEventListener("click", function () {
+    btn29.classList.toggle('clicked');
+});
+btn30.addEventListener("click", function () {
+    btn30.classList.toggle('clicked');
+});
+btn31.addEventListener("click", function () {
+    btn31.classList.toggle('clicked');
+});
+btn32.addEventListener("click", function () {
+    btn32.classList.toggle('clicked');
+});
+btn33.addEventListener("click", function () {
+    btn33.classList.toggle('clicked');
+});
+btn34.addEventListener("click", function () {
+    btn34.classList.toggle('clicked');
+});
+btn35.addEventListener("click", function () {
+    btn35.classList.toggle('clicked');
+});
+btn36.addEventListener("click", function () {
+    btn36.classList.toggle('clicked');
+});
+btn37.addEventListener("click", function () {
+    btn37.classList.toggle('clicked');
+});
+btn38.addEventListener("click", function () {
+    btn38.classList.toggle('clicked');
+});
+btn39.addEventListener("click", function () {
+    btn39.classList.toggle('clicked');
+});
+btn40.addEventListener("click", function () {
+    btn40.classList.toggle('clicked');
+});
+btn41.addEventListener("click", function () {
+    btn41.classList.toggle('clicked');
+});
+btn42.addEventListener("click", function () {
+    btn42.classList.toggle('clicked');
+});
+btn43.addEventListener("click", function () {
+    btn43.classList.toggle('clicked');
+});
+btn44.addEventListener("click", function () {
+    btn44.classList.toggle('clicked');
+});
+btn45.addEventListener("click", function () {
+    btn45.classList.toggle('clicked');
+});
+btn46.addEventListener("click", function () {
+    btn46.classList.toggle('clicked');
+});
+btn47.addEventListener("click", function () {
+    btn47.classList.toggle('clicked');
+});
+btn48.addEventListener("click", function () {
+    btn48.classList.toggle('clicked');
+});
+btn49.addEventListener("click", function () {
+    btn49.classList.toggle('clicked');
+});
+btn50.addEventListener("click", function () {
+    btn50.classList.toggle('clicked');
+});
+btn51.addEventListener("click", function () {
+    btn51.classList.toggle('clicked');
+});
+btn52.addEventListener("click", function () {
+    btn52.classList.toggle('clicked');
+});
+btn53.addEventListener("click", function () {
+    btn53.classList.toggle('clicked');
+});
+btn54.addEventListener("click", function () {
+    btn54.classList.toggle('clicked');
+});
+btn55.addEventListener("click", function () {
+    btn55.classList.toggle('clicked');
+});
+btn56.addEventListener("click", function () {
+    btn56.classList.toggle('clicked');
+});
+btn57.addEventListener("click", function () {
+    btn57.classList.toggle('clicked');
+});
+btn58.addEventListener("click", function () {
+    btn58.classList.toggle('clicked');
+});
+btn59.addEventListener("click", function () {
+    btn59.classList.toggle('clicked');
+});
+btn60.addEventListener("click", function () {
+    btn60.classList.toggle('clicked');
+});
+btn61.addEventListener("click", function () {
+    btn61.classList.toggle('clicked');
+});
+btn62.addEventListener("click", function () {
+    btn62.classList.toggle('clicked');
+});
+btn63.addEventListener("click", function () {
+    btn63.classList.toggle('clicked');
+});
+btn64.addEventListener("click", function () {
+    btn64.classList.toggle('clicked');
+});
+btn65.addEventListener("click", function () {
+    btn65.classList.toggle('clicked');
+});
+btn66.addEventListener("click", function () {
+    btn66.classList.toggle('clicked');
+});
+btn67.addEventListener("click", function () {
+    btn67.classList.toggle('clicked');
+});
+
+// 集計ボタンを押したら、クラスが付いているボタンのテキストを取り出し、テキストの頭に"@"を付ける
+let btn_aggregate = document.getElementById("btn_aggregate");
+btn_aggregate.addEventListener("click", function () {
+    let btns = document.getElementsByClassName("clicked");
+    let btns_text = [];
+    for (let i = 0; i < btns.length; i++) {
+        btns_text.push(btns[i].textContent);
+        btns_text[i] = "@" + btns_text[i] + "  ";
+    }
+    console.log(btns_text);
+
+    // 集計結果をランダムな順番でコンソールに表示する
+    let btns_text_random = [];
+    while (btns_text.length > 0) {
+        let random = Math.floor(Math.random() * btns_text.length);
+        btns_text_random.push(btns_text[random]);
+        btns_text.splice(random, 1);
+    }
+    console.log(btns_text_random);
+
+    // 集計結果を4個ずつ塊にして表示する
+    let btns_text_random_chunk = [];
+    while (btns_text_random.length > 0) {
+        btns_text_random_chunk.push(btns_text_random.splice(0, 4));
+    }
+    console.log(btns_text_random_chunk);
+
+    //上記の結果をHTMLに表示する
+    let result = document.getElementById("result");
+    result.innerHTML = "";
+    for (let i = 0; i < btns_text_random_chunk.length; i++) {
+        result.innerHTML += `<div class="result_chunk">${btns_text_random_chunk[i]}</div>`;
+    }
+});
+
+let btn_reset = document.getElementById("btn_reset");
+btn_reset.addEventListener("click", function () {
+    location.reload();
+});
+
